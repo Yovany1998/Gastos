0d69cbff9c5d684b6b7bb484a83bfb389860056e
Yovany1998 <61251212+Yovany1998@users.noreply.github.com>
2022-02-10 16:28:01 -0600

    Aplicación lista faltan los estilos

--- public/index.js
+++ public/index.js
@@ -1,1 +1,51 @@
 "use strict";
+const bAdd = document.documentElement.querySelector('#bAdd');
+const inputTitle = document.documentElement.querySelector('#title');
+//esta es otra forma
+const inputCost = document.documentElement.querySelector('#cost');
+const inputCurrency = document.documentElement.querySelector('#currency');
+const expenses = new Expenses('USD');
+//si puede ser nulo
+//bAdd?.addEventListener();
+//si estamos seguros que no es nulo
+bAdd.addEventListener('click', e => {
+    if (inputTitle.value != '' && inputCost.value != '' && !isNaN(parseFloat(inputCost.value))) {
+        const title = inputTitle.value;
+        const cost = parseFloat(inputCost.value);
+        const currency = (inputCurrency.value);
+        expenses.add({ title: title, cost: { number: cost, currency: currency } });
+        render();
+    }
+    else {
+        alert('Completa los datos completamente');
+    }
+});
+function render() {
+    let html = '';
+    expenses.getItems().forEach(item => {
+        const { id, title, cost } = item;
+        const { number, currency } = cost;
+        html += `
+        <div class="item">
+            <div><span class="currency">${currency}</span>${number} </div>
+            <div>${title}</div>
+            <div><button class="bEliminar" data-id=${id}>Eliminar</button></div>
+        </div>`;
+    });
+    //asignar al html
+    $('#iems').innerHTML = html;
+    $('#display').textContent = expenses.getTotal();
+    $$('.bEliminar').forEach(bEliminar => {
+        bEliminar.addEventListener('click', e => {
+            const id = e.target.getAttribute('data-id');
+            expenses.remove(parseInt(id));
+            render();
+        });
+    });
+}
+function $(selector) {
+    return document.querySelector(selector);
+}
+function $$(selector) {
+    return document.querySelectorAll(selector);
+}
--- src/expenses.ts
+++ src/expenses.ts
@@ -1,154 +1,129 @@
-//Definir tipo de dato
-type Currency = 'MXN' | 'USD'|'LPS';
+type Currency = 'MXN' | 'USD';
 
-//Interfaz
 interface Price{
-    number:number,
-    currency:Currency
+    number: number,
+    currency: Currency
 }
 
+
 interface ExpenseItem{
-    id:number,
-    title:string,
+    id?: number,
+    title: string,
     cost: Price
 }
 
 interface IExpenses{
-    expenses:ArrayList<ExpenseItem>,
-    finalCurrency: Currency,
-    add(item:ExpenseItem): boolean,
-    get(index:number): ExpenseItem|null,
-    getTotal(): string,
+    expenses: ArrayList<ExpenseItem>,
+    finalCurrency:Currency,
+    add(item:ExpenseItem):boolean,
+    get(index:number):ExpenseItem|null,
+    getTotal():string,
     remove(id:number):boolean
 }
 
-//declarar mi clase de arreglo
+
+
+
+class Expenses implements IExpenses{
+
+    expenses: ArrayList<ExpenseItem>;
+    finalCurrency:Currency;
+    private count:number = 0;
+
+    constructor(currency:Currency){
+        this.expenses = new ArrayList<ExpenseItem>();
+        this.finalCurrency = currency;
+    }
+
+    
+    add(item:ExpenseItem):boolean{
+        item.id = this.count;
+        this.count++;
+        this.expenses.add(item);
+        return true;
+    }
+
+    get(index:number):ExpenseItem|null{
+        return this.expenses.get(index);
+    }
+
+    getItems():ExpenseItem[]{
+        return this.expenses.getAll();
+    }
+
+    getTotal():string{
+        const total:number = this.expenses.getAll().reduce( (acc:number, item:ExpenseItem) =>{
+            return acc += this.convertCurrency(item, this.finalCurrency);
+        }, 0);
+
+        return `${this.finalCurrency} $${total.toFixed(2).toString()}`;
+    }
+
+    remove(id:number):boolean{
+        const items:ExpenseItem[] = this.getItems().filter(item =>{
+            return item.id != id
+        });
+
+        this.expenses.createFrom(items);
+        return true;
+    }
+
+    private convertCurrency(item:ExpenseItem, currency:Currency):number{
+        switch(item.cost.currency){
+            case 'USD':
+                switch(currency){
+                    case 'MXN':
+                    return item.cost.number * 22;
+                    break;
+
+                    default:
+                    return item.cost.number;
+                }
+            break;
+
+            case 'MXN':
+                switch(currency){
+                    case 'USD':
+                    return item.cost.number / 22;
+                    break;
+
+                    default:
+                    return item.cost.number;
+                }
+            break;
+
+            default:
+                return 0;
+        }
+    }
+}
+
 class ArrayList<T>{
 
-    private items:T[];
+    private items:T[] = [];
 
-        
-    constructor(){
-        this.items =[];
-    }
     add(item:T){
         this.items.push(item);
     }
 
     get(index:number):T|null{
-        const item:T[] = this.items.filter((x:T, i) => {
-            return i == index;
+        const item:T[] = this.items.filter( (x:T, i) =>{
+            return i === index;
         });
 
-        if(item.length == 0){
-            return  null;
+        if(item.length === 0){
+            return null;
         }else{
             return item[0];
         }
     }
-    createFerom(value:T[]):void{
-        this.items =[...value];
+
+    createFrom(value:T[]):void{
+        this.items = [...value];
     }
 
     getAll():T[]{
         return this.items;
-    }
-}
-
-
-class Expenses implements IExpenses{
-    expenses: ArrayList<ExpenseItem>;
-    finalCurrency: Currency;
-
-    private count = 0;
-
-    constructor(currency:Currency){
-        this.finalCurrency = currency;
-        this. expenses = new ArrayList<ExpenseItem>();
-    }
-    add(item: ExpenseItem): boolean {
-        item.id = this.count;
-        this.count++;
-        this.expenses.add(item);
-        return true;
-
-        throw new Error("Method not implemented.");
-    }
-    get(index:number): ExpenseItem | null {
-        return this.expenses.get(index);
-
-        throw new Error("Method not implemented.");
-    }
-    //refresa totdos
-    getItems():ExpenseItem[]{
-        return this.expenses.getAll()
-    }
-    getTotal(): string {
-        const total= this.getItems().reduce((acc, item) =>{
-            return acc += this.converCurrency(item, this.finalCurrency);
-        }, 0)
-
-        throw new Error("Method not implemented.");
-        return `${this.finalCurrency} $${total.toFixed(2).toString()}`;
-    }
-    remove(id: number): boolean {
-        throw new Error("Method not implemented.");
-    }
-
-    private converCurrency(item:ExpenseItem, currency:Currency):number{
-        switch(item.cost.currency){
-            //el tipo de dato Currency
-            case 'USD':
-                switch(currency){
-                    case 'MXN':
-                        return item.cost.number *22;
-                        break;
-
-                    case 'LPS':
-                        return item.cost.number *24;
-                        break;
-
-                        default:
-                            return item.cost.number;
-                }
-                break;
-                
-                case 'MXN':
-                    switch(currency){
-                        case 'USD':
-                            return item.cost.number / 22;
-                            break;
-    
-                        case 'LPS':
-                            return item.cost.number / 24;
-                            break;
-    
-                            default:
-                                return item.cost.number;
-                    }
-                    break;
-
-                    case 'LPS':
-                        switch(currency){
-                            case 'USD':
-                                return item.cost.number / 24;
-                                break;
-        
-                            case 'MXN':
-                                return item.cost.number / 24;
-                                break;
-        
-                                default:
-                                    return item.cost.number;
-                        }
-                        break;
-
-                        default:
-                            return 0;
-                    
-        }
-      
-
     }
 
 }
